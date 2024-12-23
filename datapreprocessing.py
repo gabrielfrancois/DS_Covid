90f2b4809860baa8c20e0d6c2df0563836ce4af9
yiggidr <younes.iggidr@ensae.fr>
2024-12-23 10:14:19 +0000

    functional fileopener + structured datapreprocessing

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -1,4 +1,44 @@
 import pandas as pd
+from fileopener import dataframe_creator, dataframe_etablissement
+
+# import des dataframe grâce au fileopener
+file = "donnees.txt"
+df_list = dataframe_creator(file) + [dataframe_etablissement]
+# ordre des df : hospitalisations, urgences, dépistages, espérance de vie, taux de mortalité, niveaux de vie, pauvreté monétaire, vieillissement, etablissements de santé
+df_hosp = df_list[0]
+df_urgences = df_list[1]
+df_depistage = df_list[2]
+df_espvie = df_list[3]
+df_txmort = df_list[4]
+df_nvvie = df_list[5]
+df_pauv = df_list[6]
+df_vieil = df_list[7]
+df_etab = df_list[8]
+
+# Nettoyage de la base des hospitalisations
+
+df_hosp.rename(columns = {'incid_hosp':'nb hospitalisations','incid_rea':'nb reanimations','incid_dc':'nb deces','incid_rad':'nb retour au domicile'})
+df_hosp["dep"] = df_hosp["dep"].astype(str)
+df_hosp["mois"] = df_hosp["jour"].astype(str).str[5:7]
+df_hosp["annee"] = df_hosp["jour"].astype(str).str[:4]
+#on regroupe par département par mois de l'année
+df_hosp_modifie = df_hosp.groupby(['dep','annee','mois']).sum()
+
+# Nettoyage de la base des urgences
+
+df_urgences = df_urgences[['dep',
+'date_de_passage','sursaud_cl_age_corona','nbre_pass_corona', 'nbre_pass_tot', 'nbre_pass_corona', 'nbre_pass_tot',
+'nbre_pass_corona', 'nbre_pass_tot']]
+#on remplit les valeurs non renseignées par 0 
+df_urgences.fillna(0)
+df_urgences["dep"] = df_urgences['dep'].astype(str)
+df_urgences["mois"] = df_urgences["date_de_passage"].astype(str).str[5:7]
+df_urgences["annee"] = df_urgences["date_de_passage"].astype(str).str[:4]
+df_urgences = df_urgences.groupby(['dep','annee','mois','sursaud_cl_age_corona']).sum()
+
+
+
+"""
 #ouverture du fichier et création df avec les colonnes qui nous intéressent
 url_donnees_hosp = 'https://www.data.gouv.fr/fr/datasets/r/6fadff46-9efd-4c53-942a-54aca783c30c'
 #df_hosp = pd.read_csv(url_donnees_hosp, usecols=['dep','date_de_passage','sursaud_cl_age_corona','nbre_pass_corona','nbre_hospit_corona','nbre_acte_corona','nbre_pass_tot','nbre_acte_tot'])
@@ -43,3 +83,4 @@ df_depistage["annee"] = df_depistage["jour"].astype(str).str[:4]
 df_depistage = df_depistage.groupby(['dep','annee','mois']).agg({'population':'mean',
 'patients positifs':'sum','nb patients testes':'sum'})
 print(df_depistage.head(10))
+"""
--- fileopener.py
+++ fileopener.py
@@ -11,7 +11,7 @@ def import_data_csv_to_pandas(url):
 	Output :
 	df (dataframe pandas) :  tableau pandas contenant la base de données
 	"""
-	df = pd.read_csv(url, encoding="latin1")
+	df = pd.read_csv(url, delimiter = ";", encoding="utf-8")
 	return df
 
 
@@ -37,18 +37,26 @@ def dataframe_creator(filename):
 	"""
 	file = open(filename, "r")
 	dataframe_list = []
-	try:
-		reader1 = reader(file, delimiter = ",")
-		for ligne in reader1:
-			print(ligne)
-			if ligne[0] == "csv":
-				print("csv")
-				dataframe_list.append(import_data_csv_to_pandas(ligne[1]))
-			elif ligne[0] == "xlsx":
-				dataframe_list.append(import_data_excel_to_pandas(ligne[1]))
-	finally:
-    	file.close
+	reader1 = reader(file, delimiter = ",")
+	for ligne in reader1:
+		if ligne[1] == "https://www.data.gouv.fr/fr/datasets/r/2ce43ade-8d2c-4d1d-81da-ca06c82abc68":
+			pass
+		elif ligne[0] == "csv":
+			dataframe_list.append(import_data_csv_to_pandas(ligne[1]))
+		elif ligne[0] == "xlsx":
+			dataframe_list.append(import_data_excel_to_pandas(ligne[1]))
 	return dataframe_list
+
+def dataframe_etablissement():
+    """
+    Input :
+    void
+
+    Output :
+    df_etablissement (dataframe pandas) : tableau pandas de la base des établissements de santé
+    """
+    df_etablissement = pd.read_csv("https://www.data.gouv.fr/fr/datasets/r/2ce43ade-8d2c-4d1d-81da-ca06c82abc68", delimiter = ";", encoding = "utf-8", skiprows = 1, header = None, low_memory = False)
+    return(df_etablissement)
 
 
 
