8ea9270a45a55ea737e3c115df0cbebfd621c6c3
yiggidr <younes.iggidr@ensae.fr>
2024-12-29 22:17:43 +0000

    Folder cleaning

--- stats.py
+++
@@ -1,8 +0,0 @@
-from datapreprocessing import df_final
-
-#les départements avec les plus grands ratios patients positifs/population
-df_final.nlargest(5, "taux d'incidence", keep = "all")
-
-#les départements avec les plus petits ratios patients positifs/population
-df_final.nsmallest(5, "taux d'incidence", keep = "all")
-
